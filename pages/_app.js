b5d6cc1704e1fef89cce40c21b8d5afa09621d76
Luis Landa <luislanda@Luiss-MacBook-Pro.local>
2024-01-24 14:45:17 +0100

    Redeploy cookies 6

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<GlobalStyle />
-			<Head>
+			<head>
 				<Script
 					id="cookieyes"
 					type="text/javascript"
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }) {
 					strategy="beforeInteractive"></Script>
 				<meta name="description" content=" " />
 				<title>Kanzlei Isbrandt</title>
-			</Head>
+			</head>
 
 			<Component
 				{...pageProps}
